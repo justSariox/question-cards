9f048bd15d54cce1facb3c1dc7f84eca38eba876
MalevichDi2910 <malevicdiana@gmail.com>
2023-08-25 00:38:23 +0300

    added header

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import { Button } from '@/components/ui/button'
 import { Card } from '@/components/ui/card/card.tsx'
+import { Header } from '@/components/ui/header'
 export function App() {
   return (
     <div>
+      <Header isLoggedIn={false} />
       <Card>
         <Button as={'a'} href={'https://google.com/'} variant={'link'}>
           go to google
