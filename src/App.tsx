5ceae332e180b57667c799d4fad1d390e8f44605
justSariox <SarioxSS@gmail.com>
2023-08-26 16:29:12 +0300

    added input

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,3 @@
-import { Button } from '@/components/ui/button'
-import { Card } from '@/components/ui/card/card.tsx'
 export function App() {
-  return (
-    <div>
-      <Card>
-        <Button as={'a'} href={'https://google.com/'} variant={'link'}>
-          go to google
-        </Button>
-        <div>fdikgdkfg</div>
-        <div>fdikgdkfg</div>
-        <div>fdikgdkfg</div>
-        <div>fdikgdkfg</div>
-      </Card>
-    </div>
-  )
+  return <div></div>
 }
