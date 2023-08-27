2e3b1028ba35c110b54d348ad3a22e4819a3b172
justSariox <SarioxSS@gmail.com>
2023-08-27 22:51:07 +0300

    fix textfield

--- src/App.tsx
+++ src/App.tsx
@@ -3,26 +3,5 @@ import { Card } from '@/components/ui/card/card.tsx'
 import { TextField } from '@/components/ui/textField'
 
 export function App() {
-  return (
-    <div>
-      <Card>
-        <Button as={'a'} href={'https://google.com/'} variant={'link'}>
-          go to google
-        </Button>
-        <br />
-        <Button>go to yandex</Button>
-        <br />
-        <Button variant={'secondary'}>go to yahoo</Button>
-        <br />
-        <Button variant={'tertiary'}>go to home</Button>
-        <br />
-        <Button>go away</Button>
-      </Card>
-      <Card>
-        <TextField />
-        <TextField type={'password'} />
-        <TextField type={'search'} />
-      </Card>
-    </div>
-  )
+  return <div>hello</div>
 }
